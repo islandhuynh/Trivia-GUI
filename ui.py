3518349a7fea0a079f9a56dbd19400cfcb2b03d5
Island <islandhuynh@gmail.com>
2021-07-05 03:19:53 -0400

    added get next question function to ui class

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 QUESTION_FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
-  def __init__(self):
+  def __init__(self, quiz_brain: QuizBrain):
+    self.quiz = quiz_brain
+
     self.window = Tk()
     self.window.title("Quizzler")
     self.window.config(bg=THEME_COLOR)
@@ -15,7 +18,7 @@ class QuizInterface:
     self.question_window = Canvas(width=300, height=250, highlightthickness=0)
     self.question_window.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
 
-    self.question_text = self.question_window.create_text(150, 125, font=QUESTION_FONT, text="Test", fill=THEME_COLOR)
+    self.question_text = self.question_window.create_text(150, 125, font=QUESTION_FONT, text="", fill=THEME_COLOR, width=280)
 
     true_image = PhotoImage(file="images/true.png")
     false_image = PhotoImage(file="images/false.png")
@@ -24,4 +27,10 @@ class QuizInterface:
     self.false_button = Button(image=false_image, highlightthickness=0)
     self.false_button.grid(column=1, row=2, pady=20)
 
+    self.get_next_question()
+
     self.window.mainloop()
+
+  def get_next_question(self):
+    q_text = self.quiz.next_question()
+    self.question_window.itemconfig(self.question_text, text=q_text)
