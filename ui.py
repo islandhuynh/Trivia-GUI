e1c1a9ffc475a7a7a2967d75a11c460f83473f92
Island <islandhuynh@gmail.com>
2021-07-05 03:49:26 -0400

    Added functionality for users to answer questions and update their score if correct

--- ui.py
+++ ui.py
@@ -12,7 +12,7 @@ class QuizInterface:
     self.window.title("Quizzler")
     self.window.config(bg=THEME_COLOR)
 
-    self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="#fff")
+    self.score_label = Label(text=f"Score: {self.quiz.score}", bg=THEME_COLOR, fg="#fff")
     self.score_label.grid(column=1, row=0, pady=20)
 
     self.question_window = Canvas(width=300, height=250, highlightthickness=0)
@@ -22,9 +22,9 @@ class QuizInterface:
 
     true_image = PhotoImage(file="images/true.png")
     false_image = PhotoImage(file="images/false.png")
-    self.true_button = Button(image=true_image, highlightthickness=0)
+    self.true_button = Button(image=true_image, highlightthickness=0, command=self.answer_true)
     self.true_button.grid(column=0, row=2, pady=20)
-    self.false_button = Button(image=false_image, highlightthickness=0)
+    self.false_button = Button(image=false_image, highlightthickness=0, command=self.answer_false)
     self.false_button.grid(column=1, row=2, pady=20)
 
     self.get_next_question()
@@ -34,3 +34,13 @@ class QuizInterface:
   def get_next_question(self):
     q_text = self.quiz.next_question()
     self.question_window.itemconfig(self.question_text, text=q_text)
+
+  def answer_true(self):
+    self.quiz.check_answer("True")
+    self.score_label.config(text=f"Score: {self.quiz.score}")
+    self.get_next_question()
+
+  def answer_false(self):
+    self.quiz.check_answer("False")
+    self.score_label.config(text=f"Score: {self.quiz.score}")
+    self.get_next_question()
